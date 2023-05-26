bc2f4bc200d3166b6c3462e621630b20b569a70f
Alex Younger <thealexyounger@proton.me>
2023-05-25 22:19:57 -0400

    refactor: move pages to pages controller

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -7,6 +7,8 @@ use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 use App\Models\Language;
 use App\Models\Project;
+use App\Models\Book;
+use App\Models\Category;
 
 class PagesController extends Controller
 {
@@ -14,8 +16,8 @@ class PagesController extends Controller
     {
         $languages = Language::getLanguagesWithWidths();
         return Inertia::render('Home', [
-            'canLogin' => Route::has('login'),
-            'canRegister' => Route::has('register'),
+            // 'canLogin' => Route::has('login'),
+            // 'canRegister' => Route::has('register'),
             'languages' => $languages,
             'projects' => Project::all(),
         ]);
@@ -24,5 +26,28 @@ class PagesController extends Controller
     public function resume()
     {
         return Inertia::render('Resume');
+    }
+
+    public function readingList()
+    {
+        $books = Book::with('categories')
+            ->orderBy('position', 'asc')
+            ->get();
+
+        $categories = Category::where('type', '=', 'Book::class')
+            ->get();
+
+        return Inertia::render('ReadingList', [
+            'books' => $books,
+            'categories' => $categories,
+        ]);
+    }
+
+    public function projects()
+    {
+        return Inertia::render('Projects', [
+            'projects' => Project::orderBy('position', 'asc')
+                ->get()
+        ]);
     }
 }
