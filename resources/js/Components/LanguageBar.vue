d7fbe7274de09e5601ae37cd1ab96a5ab14fa443
Alex Younger <thealexyounger@proton.me>
2024-07-28 15:09:42 -0500

    feat: add skills and bio sections to home page

--- resources/js/Components/LanguageBar.vue
+++ resources/js/Components/LanguageBar.vue
@@ -1,10 +1,10 @@
 <template>
-    <div id="language-bar" class="text-lg mx-auto pb-4 px-8">
+    <div id="language-bar" class="text-lg mx-auto">
         <div class="bar w-full mb-4">
             <template v-if="typeof languages === 'object'">
                 <template v-for="language in languages">
                     <span @mouseenter="displayHint" @mouseleave="displayHint" :style="{ width: (language.width + '%'), backgroundColor: language.color }" :id="`bar-item-${language.properties?.slug}`" class="bar-item">
-                        <p :id="`hint-${language.properties?.slug}`" class="bg-gray-300 opacity-70 invisible absolute top-16 text-black rounded text-xs z-20">
+                        <p :id="`hint-${language.properties?.slug}`" class="bg-gray-300 opacity-70 invisible absolute top-16 text-sky-50 rounded text-xs z-20">
                             {{ `${language.language}: ${language.width}%` }}
                         </p>
                     </span>
@@ -27,47 +27,29 @@
             </template>
         </ul>
         <div class="mt-6">
-            <div class="w-full flex-col-reverse flex md:flex-row justify-between items-end">
-                <div class="w-full md:w-1/2">
-                    <button @click="openDescription" class="mt-6 flex">
-                        <span class="text-burgandy">
-                            <QuestionMarkCircleIcon class="w-6 h-6 mr-1" />
-                        </span>
-                        <span class="text-burgandy">What am I looking at?</span>
-                    </button>
-                </div>
-                <div class="w-full md:w-1/2 stats text-sm md:text-right">
-                    <p>Projects Scanned: <a class="text-burgandy hover:text-red-600 font-semibold" href="https://github.com/AlextheYounga/alextheyounger-v3/blob/master/storage/data/repositories.json">{{ repoStats.count }}</a></p>
-                    <p>Projects Total Size: <a class="text-burgandy hover:text-red-600 font-semibold" href="https://github.com/AlextheYounga/alextheyounger-v3/blob/master/storage/data/repositories.json">{{ repoStats.size }}GB</a></p>
-                </div>
+            <div id="language-description" class="block" v-html="this.descriptionContent"></div>
+            <div class="w-full stats text-sm pt-3">
+                <p>Projects Scanned: <a class="text-sky-400 hover:text-blue-600 font-semibold" href="https://github.com/AlextheYounga/alextheyounger-v3/blob/master/storage/data/repositories.json">{{ repoStats.count }}</a></p>
+                <p>Projects Total Size: <a class="text-sky-400 hover:text-blue-600 font-semibold" href="https://github.com/AlextheYounga/alextheyounger-v3/blob/master/storage/data/repositories.json">{{ repoStats.size }}GB</a></p>
             </div>
-
-            <div id="language-description" :class="[descriptionOpen ? 'block' : 'hidden', 'pt-3']"></div>
         </div>
     </div>
 </template>
-  
-<script>
-import {
-    QuestionMarkCircleIcon
-} from '@heroicons/vue/24/outline';
 
-const defaultDescriptionContent = `<p class="text-sm italic pb-1">These statistics are not random. They were calculated using the <a class="text-burgandy hover:text-red-600 font-semibold" href="https://github.com/github-linguist/linguist">Github Linguist package</a> and
+<script>
+
+const defaultDescriptionContent = `<p class="text-sm italic pb-1">These statistics are not random. They were calculated using the <a class="text-sky-400 hover:text-blue-600 font-semibold" href="https://github.com/github-linguist/linguist">Github Linguist package</a> and
                     accurately
                     represent the number of bytes of code.</p>
-                <p class="text-sm italic">You can see how I did this <a class="text-burgandy hover:text-red-600 font-semibold" href="https://github.com/AlextheYounga/alextheyounger-v3/blob/master/app/Http/Services/GithubLinguistService.php">here</a>.
-                    You can even see the list of <a class="text-burgandy hover:text-red-600 font-semibold" href="https://github.com/AlextheYounga/alextheyounger-v3/blob/master/storage/data/repositories.json">repositories</a> I scanned from my machine to
-                    generate these statistics. I either legally own or have made substantial contributions to these projects. Most of them (but not all) can be found on my <a class="text-burgandy hover:text-red-600 font-semibold"
+                <p class="text-sm italic">You can see how I did this <a class="text-sky-400 hover:text-blue-600 font-semibold" href="https://github.com/AlextheYounga/alextheyounger-v3/blob/master/app/Http/Services/GithubLinguistService.php">here</a>.
+                    You can even see the list of <a class="text-sky-400 hover:text-blue-600 font-semibold" href="https://github.com/AlextheYounga/alextheyounger-v3/blob/master/storage/data/repositories.json">repositories</a> I scanned from my machine to
+                    generate these statistics. I either legally own or have made substantial contributions to these projects. Most of them (but not all) can be found on my <a class="text-sky-400 hover:text-blue-600 font-semibold"
                         href="https://github.com/AlextheYounga">Github</a>.
                 </p>`
 
 export default {
-    components: {
-        QuestionMarkCircleIcon,
-    },
     data() {
         return {
-            descriptionOpen: false,
             descriptionContent: defaultDescriptionContent,
             languages: [],
             repoStats: {
@@ -77,17 +59,12 @@ export default {
         }
     },
     methods: {
-        openDescription() {
-            console.log(this.$data.descriptionOpen)
-            this.$data.descriptionOpen = !this.$data.descriptionOpen;
-        },
         displayHint(event) {
             const id = event.target.id;
             const slug = id.split('-')[2];
             const hint = document.getElementById(`hint-${slug}`);
             hint.classList.toggle('invisible');
         },
-
     },
     mounted() {
         // Ajax fetch data
@@ -97,7 +74,6 @@ export default {
                 this.repoStats = response.data.repoStats;
 
                 const descriptionContent = response.data.content.languageDescription
-                document.getElementById(descriptionContent.html_id).innerHTML = descriptionContent.content;
             })
             .catch(error => {
                 console.error('Error fetching data:', error);
@@ -107,9 +83,9 @@ export default {
 
 };
 </script>
-  
-<style scoped>
-.language-stats #language-bar .bar {
+
+<style>
+#language-bar .bar {
     display: flex;
     height: 8px;
     overflow: hidden;
@@ -123,11 +99,11 @@ export default {
     padding: 0 4px 12px 0;
 }
 
-.language-stats #language-bar ul {
+#language-bar ul {
     list-style-type: none;
 }
 
-.language-stats #language-bar ul .octicon {
+#language-bar ul .octicon {
     fill: currentColor;
 }
 </style>
--- resources/js/Pages/Home.vue
+++ resources/js/Pages/Home.vue
@@ -4,28 +4,53 @@
 
     <AnimatedButtonMenu />
 
-    <main id="page-wrapper" class="opacity-0 transition-opacity duration-500 w-1/3 pt-32">
-        <section class="hero w-1/3 mx-auto sm:mt-0 mb-4 relative rounded">
-            <img src="/images/bridge-standing.jpg.webp" class="headshot flex mx-auto relative text-center border-sky border-2" alt="alex younger developer marketing about me" />
-        </section>
+    <main id="page-wrapper" ref="page-wrapper" class="opacity-0 transition-opacity duration-500 pt-32">
+        <div class="flex justify-between w-full">
+            <div id="home-menu" class="w-1/3">
+                <section id="hero" class="w-1/3 mx-auto sm:mt-0 mb-4 relative rounded">
+                    <img src="/images/bridge-standing.jpg.webp" class="headshot flex mx-auto relative text-center border-sky border-2" alt="alex younger developer marketing about me" />
+                </section>
 
-        <section class="title rounded relative mb-8">
-            <h1 class="text-5xl text-sky-100 text-center glow">Alex Younger</h1>
-            <p id="tagline" class="py-2 text-sky-200 text-center text-sm"></p>
-        </section>
+                <section id="title" class="rounded relative mb-8">
+                    <h1 class="text-5xl text-sky-100 text-center glow">Alex Younger</h1>
+                    <p id="tagline" class="py-2 text-sky-200 text-center text-sm">
+                        {{ $props.content?.homeTagline?.content ?? '' }}
+                    </p>
+                </section>
 
-        <section class="home-menu">
-            <ul class="text-center">
-                <li v-for="item in homeItems" class="py-1 text-xl">
-                    <template v-if="item.link">
-                        <Link :href="route(item.link)" class="text-sky-100 hover:text-sky-200 uppercase">{{ item.name }}</Link>
-                    </template>
-                    <template v-else>
-                        <p @click="item.action" class="text-sky-100 hover:text-sky-200 uppercase cursor-pointer">{{ item.name }}</p>
-                    </template>
-                </li>
-            </ul>
-        </section>
+                <section class="menu-list">
+                    <ul class="text-center">
+                        <li v-for="item in homeItems" class="py-1 text-xl">
+                            <template v-if="item.link">
+                                <Link :href="route(item.link)" class="text-sky-100 hover:text-sky-200 uppercase">{{ item.name }}</Link>
+                            </template>
+                            <template v-else>
+                                <p @click="item.action(item.id)" class="text-sky-100 hover:text-sky-200 uppercase cursor-pointer">{{ item.name }}</p>
+                            </template>
+                        </li>
+                    </ul>
+                </section>
+            </div>
+
+            <div id="hud" class="w-2/3">
+                <section id="bio" v-if="this.selected == 'bio'" class="max-w-3xl mx-auto">
+                    <div class="rounded-md p-12 border-2 border-sky-600 bg-transparent shadow shadow-sky-100">
+                        <h2 class="text-sky-300 text-2xl pb-4 font-semibold">Bio</h2>
+                        <div v-html="$props.content?.bio?.content ?? ''" class="text-sky-100"></div>
+                    </div>
+                </section>
+
+                <section id="skills" v-if="this.selected == 'skills'" class="max-w-3xl mx-auto">
+                    <div class="rounded-md p-12 border-2 border-sky-600 bg-transparent shadow shadow-sky-100">
+                        <h3 class="text-sky-300 text-2xl pb-4 font-semibold">Skills</h3>
+                        <div class="text-sky-100">
+                            <LanguageBar />
+                        </div>
+
+                    </div>
+                </section>
+            </div>
+        </div>
     </main>
 </template>
 
@@ -33,44 +58,27 @@
 import { Head } from '@inertiajs/vue3';
 import { Link } from '@inertiajs/vue3'
 import AnimatedButtonMenu from '@/Components/AnimatedButtonMenu.vue';
-// import Footer from '@/Components/Footer.vue';
-// import LanguageBar from '@/Components/LanguageBar.vue';
-// import { generateTechColors } from '@/Components/ProjectColors.vue';
+import LanguageBar from '@/Components/LanguageBar.vue';
 import { renderStarfield } from '@/three/space';
+// import Footer from '@/Components/Footer.vue';
 
-const delay = (ms) => new Promise((res) => setTimeout(res, ms));
+// Will grab this during the mounted lifecycle hook
+let pageWrapper = null;
+const sections = ['bio', 'skills'];
 
 async function reveal() {
-    await delay(1000);
-    const wrapper = document.getElementById('page-wrapper');
-    wrapper.classList.add('opacity-100')
+    const delay = 1000
+    setTimeout(() => {
+        pageWrapper.classList.add('opacity-100');
+    }, delay);
 }
-
-// Write content from db; there's probably a better way to do this
-function writeContent(content) {
-    for (const key in content) {
-        const contentItem = content[key]
-        if (contentItem && contentItem.html_id) {
-            const element = document.getElementById(contentItem.html_id)
-            if (!element) continue
-            element.innerHTML = contentItem.content
-        }
-    }
-}
-
-const homeItems = [
-    { name: 'Bio', link: false, action: 'openBio' },
-    { name: 'Skills', link: false, action: 'openSkills' },
-    { name: 'Reading List', link: 'pages.books' },
-    { name: 'Projects', link: 'pages.projects' },
-    { name: 'Explore Starfield', link: 'pages.starfield' }
-];
 
 export default {
     components: {
         Head,
         Link,
-        AnimatedButtonMenu
+        AnimatedButtonMenu,
+        LanguageBar
     },
     props: {
         content: {
@@ -82,30 +90,57 @@ export default {
             required: true,
         },
     },
-    setup() {
+    data() {
         return {
-            homeItems,
+            homeItems: [
+                { name: 'Bio', link: false, id: 'bio', action: this.openSection },
+                { name: 'Skills', link: false, id: 'skills', action: this.openSection },
+                { name: 'Reading List', link: 'pages.books' },
+                { name: 'Projects', link: 'pages.projects' },
+                { name: 'Explore Starfield', link: 'pages.starfield' }
+            ],
+            selected: 'default' // default is nothing
         }
     },
+    methods: {
+        openSection(sectionId) {
+            this.$data.selected = sectionId;
+        }
+    },
+    beforeCreate() {
+        reveal()
+    },
     mounted() {
+        pageWrapper = document.getElementById('page-wrapper');
         renderStarfield();
-        writeContent(this.$props.content);
-        reveal();
     }
 }
 </script>
 
-<style scoped>
-.hero img {
+<style>
+#page-wrapper {
+    --glowy-text-shadow: 0 0 10px #fff, 0 0 20px #fff, 0 0 30px #0ea5e9, 0 0 40px #0ea5e9, 0 0 50px #0ea5e9, 0 0 60px #0ea5e9, 0 0 70px #0ea5e9;
+}
+
+#bio a {
+    color: #38bdf8 !important;
+    text-decoration: underline;
+}
+
+#hero img {
     border-radius: 130px;
     max-width: 175px;
 }
 
-.home-menu ul li:hover {
-    text-shadow: 0 0 10px #fff, 0 0 20px #fff, 0 0 30px #0ea5e9, 0 0 40px #0ea5e9, 0 0 50px #0ea5e9, 0 0 60px #0ea5e9, 0 0 70px #0ea5e9;
+.menu-list ul li:hover {
+    text-shadow: var(--glowy-text-shadow);
+}
+
+.glow-shadow {
+    box-shadow: var(--glowy-text-shadow);
 }
 
 .glow {
-    text-shadow: 0 0 10px #fff, 0 0 20px #fff, 0 0 30px #0ea5e9, 0 0 40px #0ea5e9, 0 0 50px #0ea5e9, 0 0 60px #0ea5e9, 0 0 70px #0ea5e9;
+    text-shadow: var(--glowy-text-shadow);
 }
 </style>
