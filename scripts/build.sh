16c8f30a2cd5f76d2aad9a9124bc4483b9be7a3b
Alex Younger <thealexyounger@proton.me>
2024-09-04 17:29:31 -0500

    feat: add resume

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,8 +1,13 @@
 git fetch origin master
+wait
 git rebase origin/master
+wait
 nvm install
 wait
 yarn
+wait
 php artisan db:seed --class=RepositorySeeder
+wait
 php artisan db:seed --class=LanguageSeeder
+wait
 yarn build
