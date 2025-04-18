ba84fb8848d0c081de1e9524d0cd245ef8d1984d
Alex Younger <thealexyounger@proton.me>
2025-04-18 17:05:07 -0500

    feat: simplify 503 page and remove external libraries

--- resources/views/errors/503.blade.php
+++ resources/views/errors/503.blade.php
@@ -1,20 +1,65 @@
 <!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<html lang="en">
+
 <head>
-    @vite(['resources/css/app.css'])
-    <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+	<meta charset="UTF-8" />
+	<meta name="robots" content="noindex,nofollow,noarchive" />
+	<title>We'll Be Right Back</title>
 </head>
-<body class="bg-white antialiased h-full">
-    <div class="w-full h-full">
-        <div class="absolute left-0 right-0 top-1/4">
-            <div class="w-full">
-				<p class="text-center text-2xl -mb-12 font-bold">Neuron Activation</p>
-                <img class="mx-auto w-42 pb-6" src="/images/monkey-brain.png.webp"/>
+
+<body>
+    <div class="container">
+        <div class="absolute-container">
+            <div style="width: 100%;">
+				<p>Neuron Activation</p>
+                <img src="/images/monkey-brain.png.webp"/>
             </div>
-            <h1 class="text-center text-5xl text-gray-900">
+            <h1>
                 We'll be right back.
             </h1>
         </div>
     </div>
 </body>
+
 </html>
+
+<style>
+	body {
+		background-color: #fff;
+		color: #222;
+		font: 16px/1.5 -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif;
+		margin: 0;
+	}
+
+	.container {
+		width: 100%;
+		height: 100%;
+	}
+
+	.absolute-container {
+		position: absolute;
+		top: 50%;
+		left: 50%;
+		transform: translate(-50%, -50%);
+	}
+
+	h1 {
+		text-align: center;
+		font-size: 4rem;
+	}
+
+	p {
+		text-align: center;
+		font-size: 2rem;
+		margin-bottom: -3rem;
+		font-weight: bold;
+	}
+
+	img {
+		display: block;
+		margin-left: auto;
+		margin-right: auto;
+		width: 50%;
+		padding-bottom: 3rem;
+	}
+</style>
