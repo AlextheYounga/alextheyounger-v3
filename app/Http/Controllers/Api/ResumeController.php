602f70c59489a253fad1425030ffa636dba72163
Alex Younger <thealexyounger@proton.me>
2026-05-19 14:45:55 -0500

    feat: add resume skills to api endpoint

--- app/Http/Controllers/Api/ResumeController.php
+++ app/Http/Controllers/Api/ResumeController.php
@@ -12,6 +12,7 @@ class ResumeController extends Controller
         try {
             $record = Resume::where('hash', $hash)->firstOrFail();
             $resume = $record->toArray();
+            $resume['skills'] = collect($record->skills ?? [])->values()->all();
             $resume['projects'] = $record
                 ->projects()
                 ->get()
