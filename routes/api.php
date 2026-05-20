bf7bbcb212ddae4fc7d6df04eec3f36b8627051d
Alex Younger <thealexyounger@proton.me>
2026-05-20 16:45:21 -0500

    feat: add image caching for books and projects

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,8 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\CoverLetterController;
 use App\Http\Controllers\Api\CodingLanguageController;
+use App\Http\Controllers\Api\BookImageController;
+use App\Http\Controllers\Api\ProjectImageController;
 use App\Http\Controllers\Api\ResumeController;
 
 /*
@@ -31,6 +33,12 @@ Route::get('/resume/{hash}', [ResumeController::class, 'get'])->middleware('thro
 Route::get('/cover-letter/{hash}', [CoverLetterController::class, 'get'])->middleware(
     'throttle:50,1',
 ); // Open
+Route::get('/books/{book}/image', [BookImageController::class, 'show'])
+    ->name('api.books.image')
+    ->middleware('throttle:50,1');
+Route::get('/projects/{project}/image', [ProjectImageController::class, 'show'])
+    ->name('api.projects.image')
+    ->middleware('throttle:50,1');
 
 Route::middleware('auth:sanctum')->post('/languages', [
     CodingLanguageController::class,
