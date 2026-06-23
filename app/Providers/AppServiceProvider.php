ac2c607c2eb5f959602c86ae95863f0e7bc4b9b9
Alex Younger <thealexyounger@proton.me>
2026-06-23 01:58:21 -0500

    fix: correct forced ssl

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -20,9 +20,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        // Enforces https behind Cloudflare
-        if (app()->environment('production')) {
-            URL::forceScheme('https');
-        }
+		//
     }
 }
